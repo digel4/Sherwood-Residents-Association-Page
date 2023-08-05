1a05b33fb0d3341503b651dc9159e605c56c53c5
Oliver Fox <ofoxavondale@gmail.com>
2023-08-05 16:04:48 +0100

    pull memmbers from db and render

--- src/app/committee-members/page.tsx
+++ src/app/committee-members/page.tsx
@@ -1,8 +1,26 @@
-export default function CommitteeMembers() {
-  return (
-      <section>
-        <h1>This is the committee members page</h1>
-      </section>
+import {getCollection} from '../../firebase/firestore/getData'
 
+export default async function CommitteeMembers() {
+
+  const membersArr = await getCollection("members");
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>This is the committee members page</h1>
+
+        {
+          membersArr.map ( (member) => {
+            return (
+              <div>
+                <span>{member.data.name}</span>
+                <span>{member.data.title}</span>
+                <span>{member.data.address}</span>
+                <span>{member.data.email}</span>
+              </div>
+              
+            )
+          } )
+        }
+
+      </main>
   )
 }
