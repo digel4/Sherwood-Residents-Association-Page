31ace585f1e8208610eb600bc47834ca0f67010c
Oliver Fox <ofoxavondale@gmail.com>
2023-08-05 16:04:25 +0100

    center main

--- src/app/articles-of-association/page.tsx
+++ src/app/articles-of-association/page.tsx
@@ -1,7 +1,7 @@
 export default function ArticlesAssociation() {
   return (
-      <section>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>This is the articles association page</h1>
-      </section>
+      </main>
   )
 }
