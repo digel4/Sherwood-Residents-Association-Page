f2197055e7b58e05503adb1bc6d7288112cbe30f
Oliver Fox <ofoxavondale@gmail.com>
2023-08-05 16:05:48 +0100

    update props

--- src/app/news-&-events/NewsItem.tsx
+++ src/app/news-&-events/NewsItem.tsx
@@ -1,8 +1,9 @@
 export default function NewsItem({title, content, date}) {
+
   return (
       <section>
         <h2>{title}</h2>
-        <h5>{date}</h5>
+        <h5>{Date.parse(date).toString()}</h5>
         <p>{content}</p>
         
       </section>
