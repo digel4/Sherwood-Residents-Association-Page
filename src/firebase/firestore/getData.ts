8183a134f7a985c79cda4854ac067c3903f6ffb0
Oliver Fox <ofoxavondale@gmail.com>
2023-08-05 16:08:52 +0100

    add func to get collections from db

--- src/firebase/firestore/getData.ts
+++ src/firebase/firestore/getData.ts
@@ -1,8 +1,9 @@
 import firebase_app from "../config";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { collection, getFirestore, doc, getDoc, getDocs } from "firebase/firestore";
 
 const db = getFirestore(firebase_app)
-export default async function getDoument(collection, id) {
+async function getDoument(collection: string, id: string) {
+
     let docRef = doc(db, collection, id);
 
     let result = null;
@@ -12,8 +13,33 @@ export default async function getDoument(collection, id) {
         result = await getDoc(docRef);
     } catch (e) {
         error = e;
+
     }
 
     return { result, error };
 }
 
+async function getCollection(collectionName: string) {
+    const querySnapshot = await getDocs(collection(db, collectionName))
+    .then(   (data) => {
+        //console.log(data.docs)
+        const docData = data.docs.map( (doc) => {
+            //console.log(doc.id)
+            //const {data, id} = doc
+            const dataObj = {
+                id: doc.id,
+                data: doc.data()
+            }
+            console.log(dataObj)
+            //return doc.data()
+            return dataObj
+
+        } )
+        //console.log(docData)
+        return docData;
+    }  )
+
+    return querySnapshot
+}
+
+export {getDoument, getCollection}
