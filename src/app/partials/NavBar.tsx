4a4e6044c386b1dc085e2837e17087f9854d5b5c
Oliver Fox <ofoxavondale@gmail.com>
2023-08-05 16:06:50 +0100

    center nav

--- src/app/partials/NavBar.tsx
+++ src/app/partials/NavBar.tsx
@@ -1,8 +1,9 @@
+'use client'
 import Link from 'next/link';
 
 export default function NavBar() {
   return (
-      <section>
+      <section className="flex min-h-screen flex-col items-center justify-between p-24">
         <ul>
           <li>
             <Link href="/">Home</Link>
