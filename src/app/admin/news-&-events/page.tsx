12473e779a9dae74c4e840407ebed7274460746b
Oliver Fox <ofoxavondale@gmail.com>
2023-08-13 10:09:07 +0100

    add useEffect to get data from firebase

--- src/app/admin/news-&-events/page.tsx
+++ src/app/admin/news-&-events/page.tsx
@@ -1,26 +1,63 @@
 'use client'
 import React from "react";
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+ 
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { getCollection } from "@/firebase/firestore/getData";
 import Link from 'next/link';
+import newsItem from "@/interfaces/newsItem";
 
-async function Page() {
+
+function Page() {
   const { user } = useAuthContext()
   const router = useRouter()
-  const newsItems = await getCollection("news-&-events");
+  const [newsItems, setNewsItems] = React.useState<newsItem[]>([])
 
   React.useEffect(() => {
+
+    console.log("inside first use effect")
+
       if (user === null) {
           router.push("/admin/login")
       }
   }, [user])
 
+  React.useEffect(() => {
+    const fetchNewsItems = async () => {
+        const data = await getCollection("news-&-events");
+
+        setNewsItems(data)
+      }
+      fetchNewsItems()
+        .catch(console.error);    
+  }, [])
+
+  const renderNewsItems = () => {
+    if (newsItems.length === 0) {
+      return <div>Loading...</div>
+    }
+    return (
+      newsItems.map( (item) => {
+        return (
+            <Link href={`/admin/news-&-events/edit-item/${item.id}`} >{item.data.title}</Link>
+        )
+      } )
+    )
+  }
+
+
+
+
   return (
       <main>
           <h1>Admin Panel</h1>
-          <button >Edit Event</button>
-          <Link href={`/admin/news-&-events/edit-item/${"kfm4w352354"}`} >blog</Link>
+          <button >Edit Events</button>
+          { renderNewsItems()}
+            
+
+    
+          
       </main>
 
   );
--- src/app/news-&-events/page.tsx
+++ src/app/news-&-events/page.tsx
@@ -1,16 +1,38 @@
+'use client'
+import React from "react";
 import NewsItem from "./newsItem"
-import {getCollection} from '../../firebase/firestore/getData'
+import {getCollection} from '../../firebase/firestore/getData';
+import newsItem from "@/interfaces/newsItem";
 
 
-export default async function NewsAndEvents() {
-  const newsItemsArr = await getCollection("news-&-events")
+export default function NewsAndEvents() {
+  const [newsItems, setNewsItems] = React.useState<newsItem[]>([])
+  //const newsItemsArr = await getCollection("news-&-events")
+
+  React.useEffect(() => {
+    const fetchNewsItems = async () => {
+        const data = await getCollection("news-&-events");
+
+        setNewsItems(data)
+      }
+      fetchNewsItems()
+        .catch(console.error);    
+  }, [])
+
+  const renderNewsItems = () => {
+    if (newsItems.length === 0) {
+      return <div>Loading...</div>
+    }
+    return (
+      newsItems.map(( newsItem, i ) => <NewsItem content={newsItem.data.content} title={newsItem.data.title} date={newsItem.data.date} key={i}  />)
+    )
+    
+  }
   
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>News and Events page</h1>
-        { 
-          newsItemsArr.map(( newsItem, i ) => <NewsItem content={newsItem.data.content} title={newsItem.data.title} date={newsItem.data.date} key={i}  />)
-        }
+        {renderNewsItems()}
 
       </main>
 
